927508cf3a5cb069351594cb81df1708d173ba73
unknown <nicolasdarrieu@gmail.com>
2022-12-08 14:37:20 +0100

    feature(): video player

--- flutflyevents/lib/pages/details.dart
+++ flutflyevents/lib/pages/details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+//import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key, required this.title});
@@ -12,19 +13,85 @@ class DetailPage extends StatefulWidget {
   State<DetailPage> createState() => _DetailPageState();
 }
 
+
+
 class _DetailPageState extends State<DetailPage> {
+
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    loadVideoPlayer();
+    super.initState();
+  }
+
+    loadVideoPlayer(){
+     controller = VideoPlayerController.asset('./assets/videos/John Wick (2014).mp4');
+     controller.addListener(() {
+        setState(() {});
+     });
+    controller.initialize().then((value){
+        setState(() {});
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.message)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.mail)),
-        ],
-      ),
-    ));
+      
+         body: Column(
+           children:[
+               AspectRatio( 
+                 aspectRatio: controller.value.aspectRatio,
+                  child: VideoPlayer(controller),
+               ),
+
+               Text("Total Duration: ${controller.value.duration}"),
+
+               VideoProgressIndicator(
+                 controller, 
+                 allowScrubbing: true,
+                 colors:const VideoProgressColors(
+                     backgroundColor: Colors.redAccent,
+                     playedColor: Colors.green,
+                     bufferedColor: Colors.purple,
+                 )
+               ),
+
+               Row(
+                   children: [
+                      IconButton(
+                          onPressed: (){
+                            if(controller.value.isPlaying){
+                              controller.pause();
+                            }else{
+                              controller.play();
+                            }
+
+                            setState(() {
+                              
+                            });
+                          }, 
+                          icon:Icon(controller.value.isPlaying?Icons.pause:Icons.play_arrow)
+                     ),
+
+                     IconButton(
+                          onPressed: (){
+                            controller.seekTo(const Duration(seconds: 0));
+
+                            setState(() {
+                              
+                            });
+                          }, 
+                          icon:const Icon(Icons.stop)
+                     )
+                   ],
+               )
+           ]
+         ),
+       );
+
+
   }
 }
